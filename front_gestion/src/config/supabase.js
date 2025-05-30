14826084f49eccd97cbc3e20e296defce7d19476
jbeleno <u20212201817@usco.edu.co>
2025-05-30 15:26:09 -0500

    .env

--- front_gestion/src/config/supabase.js
+++ front_gestion/src/config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ozyjkmvsaogxrmewaafj.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96eWprbXZzYW9neHJtZXdhYWZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2MDM0NTQsImV4cCI6MjA2MzE3OTQ1NH0.7SAA-89GrcIWfo4z02OfxfbrEfAJMktz8r44meUUPfA'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
